aa9425d3ca1be72d634f269396bf5bc4a85150b2
Liane <nzisalianeamy@gmail.com>
2023-07-13 06:03:19 -0700

    KF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - Enter a printf function to output a string 
- *Return: 0
+ * main - Prints  using a printf function
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
 
