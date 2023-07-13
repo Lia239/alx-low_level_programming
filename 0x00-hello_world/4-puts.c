f021e01d6271d5a797a403b45cd499ec71349441
Liane <nzisalianeamy@gmail.com>
2023-07-13 05:39:26 -0700

    Cheshire

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - enter the program that uses puts rather than printf
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
