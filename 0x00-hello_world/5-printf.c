b5e40019508f22d9ee5423bb26f20d0a97edc46c
Liane <nzisalianeamy@gmail.com>
2023-07-13 05:44:13 -0700

    Sportsmaster

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Enter a printf function to output a string 
+ *Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
