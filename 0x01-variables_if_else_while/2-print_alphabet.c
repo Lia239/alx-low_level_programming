e8a468ae5fca6bc7541b80f629d2bc00a58009d2
Liane <nzisalianeamy@gmail.com>
2023-07-14 07:38:51 -0700

    Kent Nelson

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in small letters
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
