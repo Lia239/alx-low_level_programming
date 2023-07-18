a75a3eb7452f3ad36b7723486a76939c97f76b4b
Liane <nzisalianeamy@gmail.com>
2023-07-18 11:36:30 -0700

    Mikael

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Processes the value of an integer.
+ * @n: The integer to be processed.
+ *
+ * Return: The value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
